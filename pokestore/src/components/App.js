0febb284c1854cb9b134b7e3d2d05dde7b5da6df
Camila Chung <MilaChung@users.noreply.github.com>
2020-07-01 14:52:20 -0300

    Adding Pokemon Detail e Pokemon List in App

--- pokestore/src/components/App.js
+++ pokestore/src/components/App.js
@@ -5,6 +5,22 @@ function App() {
   return (
     <div className="ui container">
       <SearchBar />
+
+      <div className="ui grid">
+        <div className="ui row">
+          
+          <div className="eleven wide column">
+            <h1>Pokemon Detail</h1>
+            {/* <PokemonDetail /> */}
+          </div>
+
+          <div className="five wide column">
+          <h1>Pokemon List</h1>
+          {/* <PokemonList /> */}
+          </div>
+          
+        </div>
+      </div>
     </div>
   );
 }
